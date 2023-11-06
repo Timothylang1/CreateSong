8c0f8451265762fbe4e7e8bb83c3e37e10453a63
Timothylang1 <tlang@macalester.edu>
2023-11-06 14:38:39 -0600

    Cleaned up code

--- analyze_wav.py
+++ analyze_wav.py
@@ -1,30 +1,25 @@
 import numpy as np
-from scipy.io import wavfile # get the api
-from mingus.core import chords
-
+from scipy.io import wavfile
 from decimal import Decimal
 
-
 # Configuration
-FPS = 8 # How many samples we take per second
+FPS = 1 # How many samples we take per second
 
-MAX_NOTES = 6
+MAX_NOTES = 1
 
 # Names of the notes
 NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-# Or download my sample audio
-# AUDIO_FILE = "inputsongs/C Major Scale.wav"
-AUDIO_FILE = "inputsongs/Plain White T's - Hey There Delilah.wav"
-# AUDIO_FILE = "outputsongs/test3.wav"
+# Audio file pathway
+AUDIO_FILE = "inputsongs/C Major Scale"
+# AUDIO_FILE = "Chord progression"
+# AUDIO_FILE = "Plain White T's - Hey There Delilah"
 
-fs, audio = wavfile.read(AUDIO_FILE) # load the data
+fs, audio = wavfile.read("inputsongs/" + AUDIO_FILE + ".wav") # load the data
 
 # First, check if it's stereo or mono
-try:
-  audio = audio[:, 0] # Get left channel
-except:
-  pass
+try: audio = audio[:, 0] # Get left channel if stereo
+except: pass
 
 AUDIO_LENGTH = len(audio)/fs # In terms of seconds
 
@@ -40,35 +35,45 @@ def find_top_notes(fft):
 
   for note, strength in lst:
     note = int(round(freq_to_number(note)))
-    if (note not in notes): 
+    if (note not in notes): # To ensure we don't have duplicate notes
       notes.append(note)
       strengths.append(strength)
       if len(notes) == MAX_NOTES + 1: 
         break # If we have the maximum number of notes, break
   return notes, strengths
 
-# See https://newt.phys.unsw.edu.au/jw/notes.html
-def freq_to_number(f): return 69 + 12*np.log2(f/440.0) + 24
+def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
 def note_name(n): return NOTE_NAMES[n % 12] + str(int(n/12 - 1))
 
-def filter_cutoff_strength(notes, strengths):
-  # We want to find the cutoff for the strength so that the maximum number of notes we would have is MAX_NOTES
-  cutoff = strengths[0][MAX_NOTES]
-  for subStrengths in strengths:
-    cutoff = max(subStrengths[MAX_NOTES], cutoff)
-  cutoff += 1
+# def get_top_notes(notes, strengths):
+#   """
+#   This method finds the minimum cutoff frequency so that the maximum number of notes at any one time is MAX_NOTES
+#   """
+#   # We want to find the cutoff for the strength so that the maximum number of notes we would have is MAX_NOTES
+#   cutoff = strengths[0][MAX_NOTES]
+#   for subStrengths in strengths:
+#     cutoff = max(subStrengths[MAX_NOTES], cutoff)
+#   cutoff += 1
 
-  print("Cutoff: %.2E\t" % Decimal(cutoff))
+#   print("Cutoff: %.2E\t" % Decimal(cutoff))
 
-  for i in range(len(notes)):
-    current_notes = notes[i]
-    current_strengths = strengths[i]
-    for j in reversed(range(MAX_NOTES + 1)):
-      if current_strengths[j] < cutoff: current_notes.pop(j)
+#   for i in range(len(notes)):
+#     current_notes = notes[i]
+#     current_strengths = strengths[i]
+#     for j in reversed(range(MAX_NOTES + 1)):
+#       if current_strengths[j] < cutoff: current_notes.pop(j)
   
-  return notes
+#   return notes
 
+def get_top_notes(notes, strengths):
+  """
+  This method just returns the top max_notes
+  """
+  new_notes = []
+  for note in notes:
+    new_notes.append(note[0:len(note) - 1]) # remove the last note
+  return new_notes
 
 def printnotes(notes, strengths):
   for note in notes:
@@ -89,8 +94,9 @@ for i in range(FRAME_COUNT):
   all_notes.append(notes)
   all_strengths.append(strengths)
 
-filter_cutoff_strength(all_notes, all_strengths)
+get_top_notes(all_notes, all_strengths)
 
+# Handles converting frequencies to notes in midi file
 from midiutil.MidiFile import MIDIFile
 
 # create your MIDI object
@@ -100,8 +106,6 @@ channel = 0 # Only on one channel
 volume = 100
 time = 0    # time counter
 previous_notes = {}   # Tracker to see how long a note lasts
-
-
 mf.addTrackName(track, time, "Track 1")
 mf.addTempo(track, time, int(60 * FPS))
 
@@ -126,18 +130,5 @@ for notes in all_notes:
       mf.addNote(track, channel, note, time - previous_notes[note], previous_notes[note], volume)
       previous_notes[note] = 0
 
-mf.writeFile(open("outputsongs/conversion.mid", 'wb'))
-
-
-
-# # write it to disk
-# with open("outputsongs/conversion.mid", 'wb') as outf:
-#     mf.writeFile(outf)
-
-
-
-# s = find_top_notes(fft)
-# s = ["E", "C", "A"]
-# print(s)
-# chord = chords.determine(s, shorthand=True, no_inversions=False, )
-# print(chord)
+CONTEXT = "1FPS"
+mf.writeFile(open("outputsongs/" + AUDIO_FILE + " " + CONTEXT + ".mid", 'wb'))
--- analyze_wav2.py
+++
@@ -1,6 +0,0 @@
-import basic_pitch
-
-
-AUDIO_FILE = "inputsongs/Plain White T's - Hey There Delilah.wav"
-
-
